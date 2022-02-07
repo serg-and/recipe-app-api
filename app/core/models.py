b6d83557913a917a3a04b40b6194bbb064e10eb1
serg-and <serge.andriessen@gmail.com>
2022-02-07 17:49:09 +0100

    unit tests 4

--- app/core/models.py
+++ app/core/models.py
@@ -21,8 +21,8 @@ class UserManager(BaseUserManager):
             raise(ValueError('No email given'))
 
         user = self.create_user(email, password)
-        user.is_staff = True
         user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
         
         return user
@@ -34,7 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_stagg = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
